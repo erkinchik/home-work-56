e9f497790f716a0180f91b0e9e4efa9e1bc4fdeb
Erkin <erkin.berdibaev@gmail.com>
2021-03-02 02:36:28 +0300

    add render function

--- main.js
+++ main.js
@@ -6,3 +6,41 @@ let $receipt = document.querySelector('.receipt')
       let info = await result.json()
       return info
     }
+
+    async function render() {
+      let receipt = await getReceipt()
+
+      $receipt.insertAdjacentHTML(
+        "beforeend",
+        `
+          <table class = "receipt-table">
+            <tr>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+            </tr>
+            </table>
+        `
+      );
+
+
+      let $table = document.querySelector('.receipt-table')
+
+
+      receipt.forEach(({ name, ingredients, inStock }) => {
+        $table.insertAdjacentHTML(
+          "beforeend",
+
+          `
+    <tr>
+        <td>${name}</td>
+        <td>${ingredients.join(' | ')}</td>
+        <td>${inStock}</td>
+    </tr>
+
+`
+        );
+      });
+
+    }
+    render()
